84f4b419dcd521c964e94a1d3e4254177842e06a
MMBlack2 <trung.dinhquang@hotmail.com>
2022-05-26 09:35:52 -0700

    Setup

---
+++ ASP.NET MVC/Cheats/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cheats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ASP.NET MVC/Index/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Index.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
