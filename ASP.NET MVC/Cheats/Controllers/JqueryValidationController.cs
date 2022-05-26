1a64b154cdcc6bf580aa59a3b15832cb8a578d42
MMBlack2 <trung.dinhquang@hotmail.com>
2022-05-26 09:57:39 -0700

    .

---
+++ ASP.NET MVC/Cheats/Controllers/JqueryValidationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cheats.Controllers
+{
+    public class JqueryValidationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
